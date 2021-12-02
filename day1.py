6fdefb54f7046e7b5ce1019c77a8daa2ace1edda
hsmith56 <47962432+hsmith56@users.noreply.github.com>
2021-12-02 16:43:19 -0500

    tabs to spaces

--- day1.py
+++ day1.py
@@ -1,26 +1,26 @@
 import random, math
 
 def load():
-	with open('day1Input.txt') as file:
-		data = file.readlines()
-		d = [line.strip() for line in data]
-	return d
+    with open('day1Input.txt') as file:
+        data = file.readlines()
+        d = [line.strip() for line in data]
+    return d
 
 inpt = load()
 def part1(inpt):
-	inpt = [int(x) for x in inpt] 
-	count = 0
-	for index, value in enumerate(inpt[0:-1]):
-		if value < inpt[index + 1]:
-			count += 1
-	return count
+    inpt = [int(x) for x in inpt] 
+    count = 0
+    for index, value in enumerate(inpt[0:-1]):
+        if value < inpt[index + 1]:
+            count += 1
+    return count
 
 def part2(inpt):
-	inpt = [int(x) for x in inpt] 
-	count = 0
-	for index, value in enumerate(inpt[0:-2]):
-		if sum(inpt[index:index + 3]) < sum(inpt[index + 1:index + 4]):
-			count += 1
-	return count
+    inpt = [int(x) for x in inpt] 
+    count = 0
+    for index, value in enumerate(inpt[0:-2]):
+        if sum(inpt[index:index + 3]) < sum(inpt[index + 1:index + 4]):
+            count += 1
+    return count
 
 print(part1(inpt), part2(inpt))
--- day2.py
+++ day2.py
@@ -1,39 +1,39 @@
 import random, math
 
 def load():
-	with open('day2Input.txt') as file:
-		data = file.readlines()
-		d = [line.strip() for line in data]
-	return d
+    with open('day2Input.txt') as file:
+        data = file.readlines()
+        d = [line.strip() for line in data]
+    return d
 
 inpt = load()
 def part1(inpt):
-	depth = distance = 0
-	for direction in inpt:
-		if "forward" in direction:
-			value = int(direction.replace("forward ",""))
-			distance += value
-		elif "up " in direction:
-			value = int(direction.replace("up ",""))
-			depth -= value
-		elif "down" in direction:
-			value = int(direction.replace("down ",""))
-			depth += value
-	return depth * distance
+    depth = distance = 0
+    for direction in inpt:
+        if "forward" in direction:
+            value = int(direction.replace("forward ",""))
+            distance += value
+        elif "up " in direction:
+            value = int(direction.replace("up ",""))
+            depth -= value
+        elif "down" in direction:
+            value = int(direction.replace("down ",""))
+            depth += value
+    return depth * distance
 
 def part2(inpt):
-	depth = distance = aim = 0
-	for direction in inpt:
-		if "forward" in direction:
-			value = int(direction.replace("forward ",""))
-			distance += value
-			depth += value * aim
-		elif "up " in direction:
-			value = int(direction.replace("up ",""))
-			aim -= value
-		elif "down" in direction:
-			value = int(direction.replace("down ",""))
-			aim += value
-	return depth * distance
+    depth = distance = aim = 0
+    for direction in inpt:
+        if "forward" in direction:
+            value = int(direction.replace("forward ",""))
+            distance += value
+            depth += value * aim
+        elif "up " in direction:
+            value = int(direction.replace("up ",""))
+            aim -= value
+        elif "down" in direction:
+            value = int(direction.replace("down ",""))
+            aim += value
+    return depth * distance
 
 print(part1(inpt), part2(inpt))
