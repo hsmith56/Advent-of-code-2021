974138f58a953198796147b99034804ff8876c49
hsmith56 <47962432+hsmith56@users.noreply.github.com>
2021-12-02 16:08:11 -0500

    Init + day 2

---
+++ day5.py
@@ -0,0 +1,18 @@
+import random, math
+
+def load():
+	with open('day5Input.txt') as file:
+		data = file.readlines()
+		d = [line.strip() for line in data]
+	return d
+
+inpt = load()
+def part1(inpt):
+	inpt = ''.join(inpt)
+	inpt = [n for n in inpt.split(',')]
+	return 0
+
+def part2(inpt):
+	return 0
+
+print(inpt), sep ='\n')
