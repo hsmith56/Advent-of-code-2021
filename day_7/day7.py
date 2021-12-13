d68468bfb74527122737a41a1048e7b5ababb7da
hsmith56 <47962432+hsmith56@users.noreply.github.com>
2021-12-13 10:21:09 -0500

    day 7 VERY SLOW but fast to write

--- day_7/day7.py
+++ day_7/day7.py
@@ -8,9 +8,27 @@ def load():
 
 inpt = load()
 def part1(inpt):
-    return 0
+    inpt = [int(x) for x in inpt[0].split(',')]
+    dct = {i:0 for i in range(min(inpt), max(inpt)+1)}
+    for i in range(min(inpt), max(inpt)+1):
+        for element in inpt:
+            if dct.get(i):
+                dct[i] += abs(i - element)
+            else:
+                dct[i] = abs(i - element)
+    min_value = min(dct.values())
+    return min_value
 
 def part2(inpt):
-    return 0
+    inpt = [int(x) for x in inpt[0].split(',')]
+    dct = {i:0 for i in range(min(inpt), max(inpt)+1)}
+    for i in range(min(inpt), max(inpt)+1):
+        for element in inpt:
+            if dct.get(i):
+                dct[i] += sum([i for i in range(abs(i - element)+1)])
+            else:
+                dct[i] = sum([i for i in range(abs(i - element)+1)])
+    min_value = min(dct.values())
+    return min_value
 
 print(f'Part1: {part1(inpt)}\nPart2: {part2(inpt)}')
